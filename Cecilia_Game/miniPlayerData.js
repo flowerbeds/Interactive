df5cc0b7128866744ef580fd9828c668f6c93121
flowerbeds <francesmaurer0@gmail.com>
2020-04-29 15:44:26 -0700

    update cecilia game

---
+++ Cecilia_Game/miniPlayerData.js
@@ -0,0 +1,15 @@
+klynt.miniPlayerData = {
+    "downloadAppWording": "Download App",
+    "title": "Cecilia",
+    "launchAppWording": "Then Launch Project",
+    "resumePlaybackWording": "Resume playback?",
+    "redirectToMobileApp": "auto",
+    "thanksForWatchingWording": "Thanks for watching!",
+    "thumbnail": "Medias/Posters/opening.mp4.jpg",
+    "noWording": "No",
+    "url": "",
+    "yesWording": "Yes",
+    "description": "Narrative Project",
+    "fullscreenInfoWording": "This program will launch in fullscreen",
+    "analyticsKey": ""
+}
